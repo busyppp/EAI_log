efa1fa8c513b9cbc904e418b6d56744f8ccd615f
SJY <sjy1236@futuregen.co.kr>
2021-08-31 17:39:13 +0900

    pgsql 조회 및 연동 추가(tran_id, if_id)

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
+from os.path import exists
 from flask import Flask, render_template, request
 import psycopg2
-
-
-
-#sql_log = "select * from public.eai_trans_log order by proc_time desc"
-#cur.execute(sql_log)
-#log_data_list = cur.fetchall()
-
-#sql_mon = "select * from public.eai_trans_mon order by proc_time desc"
-#cur.execute(sql_mon)
-#mon_data_list = cur.fetchall()
-
 
 app = Flask(__name__)
 
@@ -18,16 +8,21 @@ app = Flask(__name__)
 def eai_log():
     if request.method == 'POST':
         tran_id = request.form['tran_id']
+        if_id = request.form['if_id']
         con = psycopg2.connect(host='localhost', dbname='postgres', user='postgres', password='1111', port='5432')
+
         cur1 = con.cursor()
-        cur1.execute(f"select * from public.eai_trans_log where tran_id like ('%{tran_id}%') order by proc_time desc")
-        #cur1.execute(f"select * from public.eai_trans_log order by proc_time desc")
+        cur1.execute(f"select * from public.eai_trans_log where tran_id like ('%{tran_id}%') and if_id like ('%{if_id}%') order by proc_time desc")
+
         log_data_list = cur1.fetchall()
+
         cur2 = con.cursor()
-        cur2.execute(f"select * from public.eai_trans_mon where tran_id like ('%{tran_id}%') order by proc_time desc")
-        #cur2.execute(f"select * from public.eai_trans_mon order by proc_time desc")
+        cur2.execute(f"select * from public.eai_trans_mon where tran_id like ('%{tran_id}%') and if_id like ('%{if_id}%') order by proc_time desc")
+
         mon_data_list = cur2.fetchall()
+
         return render_template("pgsql.html", log_data_list=log_data_list, mon_data_list=mon_data_list)
+
     else:
         return render_template("pgsql.html")
 
